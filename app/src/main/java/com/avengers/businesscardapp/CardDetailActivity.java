a68b8566d700b2d07dd58ac0342ac51df182d2f5
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-06 02:37:15 -0700

    Code setup toolbar

--- app/src/main/java/com/avengers/businesscardapp/CardDetailActivity.java
+++ app/src/main/java/com/avengers/businesscardapp/CardDetailActivity.java
@@ -1,13 +1,37 @@
 package com.avengers.businesscardapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class CardDetailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView title;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_card_detail);
+        mContext = CardDetailActivity.this;
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        title = findViewById(R.id.toolbar_title);
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        title.setText(getString(R.string.app_name));
     }
 }
