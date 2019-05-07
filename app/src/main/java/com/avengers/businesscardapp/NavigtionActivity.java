3e90ffb7fc51bc0957f167c4025af4d955238d64
Shilpi Kumari <shilpi.kumari2206@gmail.com>
2019-05-07 16:36:54 -0700

    fixing conflict

--- app/src/main/java/com/avengers/businesscardapp/NavigtionActivity.java
+++ app/src/main/java/com/avengers/businesscardapp/NavigtionActivity.java
@@ -58,17 +58,11 @@ public class NavigtionActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_addCard) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
