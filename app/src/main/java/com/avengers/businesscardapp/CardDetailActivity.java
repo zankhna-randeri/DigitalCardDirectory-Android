1859dcd9bc06ac80743aefde1335695008d1b34b
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-07 11:22:48 -0700

    Handle tab button click

--- app/src/main/java/com/avengers/businesscardapp/CardDetailActivity.java
+++ app/src/main/java/com/avengers/businesscardapp/CardDetailActivity.java
@@ -1,19 +1,37 @@
 package com.avengers.businesscardapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class CardDetailActivity extends AppCompatActivity {
+import com.avengers.businesscardapp.dto.Card;
+import com.avengers.businesscardapp.fragment.ContactsFragment;
+import com.avengers.businesscardapp.util.Constants;
+
+public class CardDetailActivity extends AppCompatActivity implements
+        View.OnClickListener, ContactsFragment.OnFragmentInteractionListener {
 
     private Toolbar toolbar;
     private TextView title;
     private Context mContext;
+    private Button btnContacts;
+    private Button btnCard;
+    private Button btnNotes;
+
+    private Card card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +39,14 @@ public class CardDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_detail);
         mContext = CardDetailActivity.this;
         initView();
+        fetchCardDetail(getIntent());
+        Fragment contactsFragment = ContactsFragment.newInstance(
+                new Card("Jhon Deo", "Abby Europe",
+                        "abby@gmail.com", "+1 002-030-3040",
+                        "notes..."));
+        setDefaultFragment(contactsFragment);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -43,10 +68,70 @@ public class CardDetailActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_contacts:
+                handleContactsClick();
+                break;
+            case R.id.btn_card:
+                handleCardClick();
+                break;
+            case R.id.btn_notes:
+                handleNotesClick();
+                break;
+        }
+    }
+
+    private void fetchCardDetail(Intent intent) {
+        if (intent.getExtras() != null &&
+                getIntent().getExtras().containsKey(Constants.EXTRA_CARD_DETAIL)) {
+            card = getIntent().getExtras().getParcelable(Constants.EXTRA_CARD_DETAIL);
+            Fragment contactsFragment = ContactsFragment.newInstance(card);
+            setDefaultFragment(contactsFragment);
+        }
+    }
+
     private void initView() {
         toolbar = findViewById(R.id.toolbar);
         title = findViewById(R.id.toolbar_title);
+        btnCard = findViewById(R.id.btn_card);
+        btnContacts = findViewById(R.id.btn_contacts);
+        btnNotes = findViewById(R.id.btn_notes);
         setUpToolbar();
+        btnNotes.setOnClickListener(this);
+        btnContacts.setOnClickListener(this);
+        btnCard.setOnClickListener(this);
+    }
+
+    private void handleCardClick() {
+        setSelected(btnCard);
+        deselectButton(btnNotes);
+        deselectButton(btnContacts);
+
+    }
+
+    private void handleContactsClick() {
+        setSelected(btnContacts);
+        deselectButton(btnCard);
+        deselectButton(btnNotes);
+    }
+
+    private void handleNotesClick() {
+        setSelected(btnNotes);
+        deselectButton(btnContacts);
+        deselectButton(btnNotes);
+    }
+
+    private void deselectButton(Button tabButton) {
+        tabButton.setSelected(false);
+        tabButton.setTextColor(ContextCompat.getColor(mContext, android.R.color.black));
+    }
+
+    private void setSelected(Button tabButton) {
+        tabButton.setSelected(true);
+        tabButton.setTextColor(ContextCompat.getColor(mContext, android.R.color.white));
     }
 
     private void setUpToolbar() {
@@ -56,5 +141,21 @@ public class CardDetailActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         title.setText(getString(R.string.txt_card_detail));
+    }
+
+    private void setDefaultFragment(Fragment defaultFragment) {
+        replaceFragment(defaultFragment);
+    }
+
+    private void replaceFragment(Fragment defaultFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_card_detail, defaultFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
