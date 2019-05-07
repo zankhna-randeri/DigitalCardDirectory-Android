6150f6b15b113f54903638efd19234611eefb59e
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-07 11:22:11 -0700

    Fragment to display contact details of card

---
+++ app/src/main/java/com/avengers/businesscardapp/fragment/ContactsFragment.java
@@ -0,0 +1,107 @@
+package com.avengers.businesscardapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.avengers.businesscardapp.R;
+import com.avengers.businesscardapp.dto.Card;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ContactsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ContactsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactsFragment extends Fragment {
+
+    // the fragment initialization parameters
+    private static final String ARG_CONTACTS = "arg_contacts";
+
+    private TextView txtName;
+    private TextView txtOrganization;
+
+    private Card card;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * create a new instance of this fragment using the provided parameters.
+     *
+     * @param card Card detail parameter
+     * @return A new instance of fragment ContactsFragment.
+     */
+    public static ContactsFragment newInstance(Card card) {
+        ContactsFragment fragment = new ContactsFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_CONTACTS, card);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            card = getArguments().getParcelable(ARG_CONTACTS);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        txtName = view.findViewById(R.id.txt_name);
+        txtOrganization = view.findViewById(R.id.txt_org);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
