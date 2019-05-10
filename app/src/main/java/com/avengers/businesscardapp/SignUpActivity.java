51054d1ca156c7c09c64919e13cd8eb2cab34ab4
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-09 17:05:06 -0700

    Implemented signup api call

--- app/src/main/java/com/avengers/businesscardapp/SignUpActivity.java
+++ app/src/main/java/com/avengers/businesscardapp/SignUpActivity.java
@@ -1,93 +1,181 @@
 package com.avengers.businesscardapp;
 
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.avengers.businesscardapp.db.DataControllerBusinessCard;
+import com.avengers.businesscardapp.dto.SignupUser;
+import com.avengers.businesscardapp.util.NetworkHelper;
+import com.avengers.businesscardapp.webservice.BusinessCardWebservice;
+import com.avengers.businesscardapp.webservice.GenericResponse;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import java.io.IOException;
 
-public class SignUpActivity extends AppCompatActivity {
+import retrofit2.Call;
 
-    String fName, lName, mailId, pwd;
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar toolbar;
+    private TextView title;
+    private Context mContext;
+    private EditText edtFname, edtLname, edtEmail, edtPassword, edtConfirmPassword;
+    private Button btnSubmit, btnCancel;
+    private String fName, lName, emailId, pwd, confirmPwd;
+    private final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mContext = SignUpActivity.this;
+        initView();
     }
 
-    public void saveCustomerDetails(View view) {
-        EditText firstName = (EditText) findViewById(R.id.firstName);
-        fName = firstName.getText().toString();
-        EditText lastName = (EditText) findViewById(R.id.lastName);
-        lName = lastName.getText().toString();
-        EditText emailId = (EditText) findViewById(R.id.emailId);
-        mailId = emailId.getText().toString();
-        EditText password = (EditText) findViewById(R.id.password);
-        pwd = password.getText().toString();
-        EditText confirmPassword = (EditText) findViewById(R.id.confirmPassword);
-        String confirmPwd = confirmPassword.getText().toString();
+    public void saveCustomerDetails() {
+        fName = edtFname.getText().toString();
+        lName = edtLname.getText().toString();
+        emailId = edtEmail.getText().toString();
+        pwd = edtPassword.getText().toString();
+        confirmPwd = edtConfirmPassword.getText().toString();
 
-        if (fName.equalsIgnoreCase("") || lName.equalsIgnoreCase("") ||
-                mailId.equalsIgnoreCase("") || pwd.equalsIgnoreCase("") ||
-                confirmPwd.equalsIgnoreCase("")) {
-            Context context = getApplicationContext();
-            CharSequence text = getString(R.string.enter_all_signup_fields);
-            int duration = Toast.LENGTH_LONG;
-            Toast.makeText(context, text, duration).show();
+        if (fName.trim().isEmpty() || lName.trim().isEmpty() ||
+                emailId.trim().isEmpty() || pwd.trim().isEmpty() ||
+                confirmPwd.trim().isEmpty()) {
+            showMsg(getResources().getString(R.string.enter_all_signup_fields));
+        } else if (!pwd.equalsIgnoreCase(confirmPwd)) {
+            showMsg(getResources().getString(R.string.password_match));
         } else {
-            if (pwd.equalsIgnoreCase(confirmPwd)) {
-                signUpValidation();
-            } else {
-                Context context = getApplicationContext();
-                CharSequence text = getString(R.string.password_match);
-                int duration = Toast.LENGTH_LONG;
-                Toast.makeText(context, text, duration).show();
-            }
+            SignupUser user = new SignupUser();
+            user.setFirstName(fName);
+            user.setLastName(lName);
+            user.setEmailId(emailId);
+            user.setPassword(pwd);
+            new SignUpTask(SignUpActivity.this, user).execute();
         }
     }
 
-    public void signUpValidation() {
-        DataControllerBusinessCard dataController = new DataControllerBusinessCard(getBaseContext());
-        dataController.open();
+//    public void signUpValidation() {
 
-        //Check if whether the record already exists
-        String existingEmailId = dataController.retrieveCustomerInfo(mailId);
 
-        if (existingEmailId != null && existingEmailId != "") {
-            Context context = getApplicationContext();
-            CharSequence text = getString(R.string.customer_exists);
-            int duration = Toast.LENGTH_LONG;
-            Toast.makeText(context, text, duration).show();
-        } else {
-            long retValue = dataController.insert(fName, lName, mailId, pwd);
+//        DataControllerBusinessCard dataController = new DataControllerBusinessCard(getBaseContext());
+//        dataController.open();
+//
+//        //Check if whether the record already exists
+//        String existingEmailId = dataController.retrieveCustomerInfo(emailId);
+//
+//        if (existingEmailId != null && existingEmailId != "") {
+//            Context context = getApplicationContext();
+//            CharSequence text = getString(R.string.customer_exists);
+//            int duration = Toast.LENGTH_LONG;
+//            Toast.makeText(context, text, duration).show();
+//        } else {
+//            long retValue = dataController.insert(fName, lName, emailId, pwd);
+//
+//            if (retValue != -1) {
+//                Context context = getApplicationContext();
+//                CharSequence text = getString(R.string.success_msg);
+//                int duration = Toast.LENGTH_LONG;
+//                Toast.makeText(context, text, duration).show();
+//            }
+//
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        }
+//        dataController.close();
+//    }
 
-            if (retValue != -1) {
-                Context context = getApplicationContext();
-                CharSequence text = getString(R.string.success_msg);
-                int duration = Toast.LENGTH_LONG;
-                Toast.makeText(context, text, duration).show();
-            }
-
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-        dataController.close();
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        title = findViewById(R.id.toolbar_title);
+        edtFname = findViewById(R.id.edt_first_name);
+        edtLname = findViewById(R.id.edt_last_name);
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        edtConfirmPassword = findViewById(R.id.edt_confirm_password);
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnCancel = findViewById(R.id.btn_cancel);
+        setUpToolbar();
+        btnSubmit.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
     }
 
-    public void signupCancel(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+    private void showMsg(String msg) {
+        Toast.makeText(getApplicationContext(), msg,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        title.setText(getString(R.string.txt_sign_up));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_submit:
+                saveCustomerDetails();
+                break;
+            case R.id.btn_cancel:
+                finish();
+                break;
+        }
+    }
+
+    private class SignUpTask extends AsyncTask<Void, GenericResponse, GenericResponse> {
+
+        private Context mContext;
+        private SignupUser user;
+
+        public SignUpTask(Context mContext, SignupUser user) {
+            this.mContext = mContext;
+            this.user = user;
+        }
+
+        @Override
+        protected GenericResponse doInBackground(Void... voids) {
+            if (NetworkHelper.hasNetworkAccess(mContext)) {
+                BusinessCardWebservice webservice = BusinessCardWebservice
+                        .retrofit.create(BusinessCardWebservice.class);
+                Call<GenericResponse> call = webservice.registerUser(user);
+                try {
+                    GenericResponse response = call.execute().body();
+                    return response;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "SignUpTask: " + e.getMessage());
+                    return null;
+                }
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(GenericResponse response) {
+            super.onPostExecute(response);
+            if (response != null && response.getMessage() != null) {
+                if (response.getResponseCode() != 200) {
+                    showMsg(response.getMessage());
+                } else {
+
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        }
     }
 }
 
