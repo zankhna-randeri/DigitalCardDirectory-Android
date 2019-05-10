7d2b041a79f388d58650a2f2aa5b00db36663948
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-09 18:43:57 -0700

    Class to handle login response

---
+++ app/src/main/java/com/avengers/businesscardapp/webservice/LoginResponse.java
@@ -0,0 +1,49 @@
+package com.avengers.businesscardapp.webservice;
+
+public class LoginResponse {
+
+    private String message;
+    private Integer responseCode;
+    private String firstName;
+    private String lastName;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(Integer responseCode) {
+        this.responseCode = responseCode;
+    }
+}
