109bae7f78b6d4674dca3cd674ec43990a8cc685
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-09 17:08:01 -0700

    Added SignUp request

--- app/src/main/java/com/avengers/businesscardapp/webservice/BusinessCardWebservice.java
+++ app/src/main/java/com/avengers/businesscardapp/webservice/BusinessCardWebservice.java
@@ -1,13 +1,13 @@
 package com.avengers.businesscardapp.webservice;
 
+import com.avengers.businesscardapp.dto.SignupUser;
+
 import okhttp3.MultipartBody;
-import okhttp3.Request;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
@@ -15,15 +15,18 @@ import retrofit2.http.Part;
 
 public interface BusinessCardWebservice {
 
-    //    String BASE_URL = "https://opentdb.com/";
-    //String BASE_URL = "http://10.0.2.2:8080";
-    String BASE_URL = "http://10.15.232.131:8080";
+    String BASE_URL = "http://10.0.2.2:8080";
+//    String BASE_URL = "http://10.15.232.131:8080";
 
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
+
+    @POST("/userSignup")
+    Call<GenericResponse> registerUser(@Body SignupUser userRequest);
+
 
     @GET("/getCloudFrontUrl")
     Call<GenericResponse> getCardUrl();
@@ -32,6 +35,6 @@ public interface BusinessCardWebservice {
     @POST("/cmpe277AvengersNER")
     Call<GenericResponse> uploadCard
             (@Part MultipartBody.Part file,
-            @Part("emailId") RequestBody emailId);
+             @Part("emailId") RequestBody emailId);
 
 }
