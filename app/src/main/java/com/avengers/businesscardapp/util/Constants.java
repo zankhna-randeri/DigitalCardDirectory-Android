0f6fcfa04050c1ca78268450cbebc73ae0e7fd42
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-09 17:07:07 -0700

    Response code

--- app/src/main/java/com/avengers/businesscardapp/util/Constants.java
+++ app/src/main/java/com/avengers/businesscardapp/util/Constants.java
@@ -3,4 +3,6 @@ package com.avengers.businesscardapp.util;
 public class Constants {
 
     public static final String EXTRA_CARD_DETAIL = "card_detail";
+    public static final int RESPONSE_FAIL = 417;
+    public static final int RESPONSE_OK = 200;
 }
