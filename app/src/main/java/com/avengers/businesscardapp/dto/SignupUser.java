7c3faf1a7afb88daa0479c3629cd70e274af3bb7
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-09 17:06:31 -0700

    User POJO class

---
+++ app/src/main/java/com/avengers/businesscardapp/dto/SignupUser.java
@@ -0,0 +1,53 @@
+package com.avengers.businesscardapp.dto;
+
+public class SignupUser {
+
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String password;
+
+    public SignupUser() {
+    }
+
+    public SignupUser(String firstName, String lastName, String emailId, String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.password = password;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
