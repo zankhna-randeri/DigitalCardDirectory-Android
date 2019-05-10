aeed10a16b07d8cbefbbe2f73ea5daf4b3b94db5
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-10 12:25:49 -0700

    Added read storage permission

--- app/src/main/java/com/avengers/businesscardapp/NavigationActivity.java
+++ app/src/main/java/com/avengers/businesscardapp/NavigationActivity.java
@@ -1,83 +1,78 @@
 package com.avengers.businesscardapp;
 
-import android.Manifest;
-import android.content.ContentValues;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Layout;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.avengers.businesscardapp.fragment.AddCardFragment;
 import com.avengers.businesscardapp.fragment.MyCardFragment;
 
 
 public class NavigtionActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, MyCardFragment.OnFragmentInteractionListener, AddCardFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MyCardFragment.OnFragmentInteractionListener, AddCardFragment.OnFragmentInteractionListener {
 
-    private static final int PERMISSION_CODE = 500;
-    private static final int IMAGE_CAPTURE_CODE = 1000;
-    TextView name, email;
-    ImageView imageView;
+    private Toolbar toolbar;
+    private TextView name, email;
+    private TextView title;
+    private String emailId, firstName, lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigtion);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        emailId = sharedPrefs.getString("Email_Id", "");
+        firstName = sharedPrefs.getString("First_Name", "");
+        lastName = sharedPrefs.getString("Last_Name", "");
+        initView();
+    }
 
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        title = findViewById(R.id.toolbar_title);
+//        setSupportActionBar(toolbar);
+        setUpToolbar();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String emailId = sharedPrefs.getString("Email_Id", "");
-        String firstName = sharedPrefs.getString("First_Name", "");
-        String lastName = sharedPrefs.getString("Last_Name", "");
-
         View header = navigationView.getHeaderView(0);
-        name = (TextView) header.findViewById(R.id.text_name);
-        email = (TextView) header.findViewById(R.id.text_email);
-        name.setText(firstName+" "+lastName);
+        name = header.findViewById(R.id.text_name);
+        email = header.findViewById(R.id.text_email);
+        name.setText(firstName + " " + lastName);
         email.setText(emailId);
-
-
     }
 
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        title.setText(getString(R.string.app_name));
+    }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -90,14 +85,12 @@ public class NavigtionActivity extends AppCompatActivity
             Fragment addCardFragment = AddCardFragment.newInstance(emailId);
             setDefaultFragment(addCardFragment);
         } else if (id == R.id.nav_gallery) {
-            Fragment myCarsdFragment = MyCardFragment.newInstance(emailId);
-            setDefaultFragment(myCarsdFragment);
-
-        } else if (id == R.id.nav_share) {
+            Fragment myCardsFragment = MyCardFragment.newInstance(emailId);
+            setDefaultFragment(myCardsFragment);
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
