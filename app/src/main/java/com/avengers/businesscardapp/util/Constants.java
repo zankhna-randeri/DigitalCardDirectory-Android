10fb372302b9101249fd50ed94c488319edf8539
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-11 17:45:45 -0700

    Added constants

--- app/src/main/java/com/avengers/businesscardapp/util/Constants.java
+++ app/src/main/java/com/avengers/businesscardapp/util/Constants.java
@@ -3,6 +3,7 @@ package com.avengers.businesscardapp.util;
 public class Constants {
 
     public static final String EXTRA_CARD_DETAIL = "card_detail";
+    public static final String EXTRA_IMG_URI = "img_uri";
     public static final int RESPONSE_FAIL = 417;
     public static final int RESPONSE_OK = 200;
 }
